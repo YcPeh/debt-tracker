51889be4d2f93cc3f0db9ab1f2544d676ff0a508
YcPeh <104683280+YcPeh@users.noreply.github.com>
2023-07-14 06:21:12 +0800

    MongoDB testing with Postman with workable image upload but code not optimised
    Former-commit-id: b7f35c8374587bf3084a7959c2ab79b729077fec

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import './App.css';
+import { MainContainer } from './component/MainContainer';
+import { useDispatch } from 'react-redux';
+
+
+function App() {
+  return (
+    <>
+      <MainContainer/>
+    </>
+  );
+}
+
+export default App;
