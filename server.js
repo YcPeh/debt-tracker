f576209f2afda4e6b0fb98b6d1084b6eee2c38b0
YcPeh <104683280+YcPeh@users.noreply.github.com>
2023-07-15 01:37:30 +0800

    Able to bring data from MongoDB to dislpay in React but not included with the exisiting function of loading user info accordingly yet
    Former-commit-id: 65dc6116db3b9bbc3ee40f6520e928f3d5646445

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const multer = require('multer');
+const cors = require('cors');
+
 
 const connectDB = require('./config/db');
 const User = require('./routes/user');
 
 const app = express();
+
+app.use(cors());
 
 // Connect to MongoDB
 connectDB();
@@ -17,7 +21,8 @@ connectDB();
 // app.use(express.json());
 
 // app.get('/',(req,res) => res.send('Hellooo'));
-app.use('/api/v1/User', User);
+// app.use('/api/v1/User', User);
+app.use(User);
 // app.use('/api/v1/User', upload.single('image'), User);;
 
 
