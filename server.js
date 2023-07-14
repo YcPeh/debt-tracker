326e51d7ccbc2ee7570759a33f79686363b95231
YcPeh <104683280+YcPeh@users.noreply.github.com>
2023-07-14 19:43:58 +0800

    refactor addUser image upload to mongodb and also add deleteUser from db
    Former-commit-id: c58d4582bf04cb0b788d2be31267c6fa1fcd03c3

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// https://youtu.be/GyzC-30Bqfc
+// https://youtu.be/NzROCbkvIE0 
 // app.js or server.js
 const express = require('express');
 const dotenv = require('dotenv');
@@ -12,50 +14,12 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
-// multer
-const UserModel = require('./models/User');
-const Storage = multer.diskStorage({
-  destination: 'uploads',
-  filename: (req,file,cb) => {
-    cb(null, file.originalname);
-  },
-});
-// const upload = multer({
-//   storage: Storage
-// }).single('image');
-// app.post('/api/v1/User',(req,res)=>{
-//   upload(req,res,(err)=>{
-//     if (err) {
-//       console.log(err)
-//     } else {
-//       const newUser = new UserModel({
-//         name: req.body.name,
-//         image: {
-//           data: req.file.filename,
-//           contentType: 'image/png',
-//         }
-//       })
-//       newUser.save()
-//         .then(() => res.send("successfully uploaded"))
-//         .catch((err) => console.log(err));
-//     }
-//   })
-// })
-
-// // Create storage configuration for multer
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage: storage });
-
-// Create storage configuration for multer
-const upload = multer({ storage: Storage }).single('image');
-
 // app.use(express.json());
 
 // app.get('/',(req,res) => res.send('Hellooo'));
 app.use('/api/v1/User', User);
 // app.use('/api/v1/User', upload.single('image'), User);;
 
-// Your other server configurations and routes
 
 app.listen(5000, () => {
   console.log('Server running on port 5000');
