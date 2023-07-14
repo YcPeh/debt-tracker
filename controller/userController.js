c58d4582bf04cb0b788d2be31267c6fa1fcd03c3
YcPeh <104683280+YcPeh@users.noreply.github.com>
2023-07-14 19:43:58 +0800

    refactor addUser image upload to mongodb and also add deleteUser from db

--- controller/userController.js
+++ controller/userController.js
@@ -30,58 +30,44 @@ exports.getUser = async (req, res, next) => {
 };
 exports.addUser = async (req, res, next) => {
     // res.send('POST user');
-
-    // JSON
-    // try {
-    //     console.log(req.body)
-    //     const {name, image} = req.body;
-    //     const user = await UserModel.create(req.body);
-    //     return res.send(201).json({
-    //         success: true,
-    //         data: user,
-    //     })
-    // } catch (error) {
-    //     console.log(error);
-    // }
-
-    // // form-data with image
-    // try {
-    //     const { name } = req.body;
-    //     const user = await UserModel.create({
-    //         name: name,
-    //         image: req.file.buffer, // Store the file buffer in the 'image' field
-    //     });
-    //     return res.status(201).json({
-    //         success: true,
-    //         data: user,
-    //     });
-
-    // } catch (error) {
-    //     console.log(error)
-    //     return res.status(500).json({
-    //         success: false,
-    //         error: 'Server Error',
-    //     });
-    // }
-
-    upload(req,res,(err)=>{
-        if (err) {
-            console.log('hihi')
-          console.log(err)
-        } else {
-          const newUser = new UserModel({
-            name: req.body.name,
-            image: {
-              data: req.file.filename,
-              contentType: 'image/png',
-            }
-          })
-          newUser.save()
-            .then(() => res.send("successfully uploaded"))
-            .catch((err) => console.log(err));
-        }
+    try {
+      upload(req,res,(err)=>{
+        const newUser = new UserModel({
+          name: req.body.name,
+          image: {
+            data: req.file.filename,
+            contentType: 'image/png',
+          }
+        })
+        newUser.save()
+          .then(() => res.send("successfully uploaded"))
+          .catch((err) => console.log(err));
       })
+    } catch (error) {
+      console.log(err)
+    }
 };
+
 exports.deleteUser = async (req, res, next) => {
-    res.send('DELETE user');
+    // res.send('DELETE user');
+    try {
+      const deleteUser = await UserModel.findByIdAndRemove(req.params.id)
+      if (!deleteUser) {
+        return res.status(404).json({
+          success: false,
+          error: 'No transaction found'
+        });
+      }
+      // await deleteUser.remove();
+      return res.status(200).json({
+        success: true,
+        data: {}
+      })
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({
+        success: false,
+        error: 'Server Error',
+    });
+    }
 };
